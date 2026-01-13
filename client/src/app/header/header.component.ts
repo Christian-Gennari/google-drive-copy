03899eb6b0b3d1cfbb99099f5d911230860a3fa2
skogsfrans <156783701+skogsfrans@users.noreply.github.com>
2026-01-13 15:54:06 +0100

    Add icons to header in template

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {SearchbarComponent} from '../components/searchbar/searchbar.component';
+import { NavButtonComponent } from '../components/nav-button/nav-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [SearchbarComponent],
+  imports: [SearchbarComponent, NavButtonComponent],
   template: `
   <header>
     <div class="brand">
@@ -11,10 +12,22 @@ import {SearchbarComponent} from '../components/searchbar/searchbar.component';
     <h1>Drive</h1>
     </div>
     <app-searchbar class="header-search"></app-searchbar>
+    <div class="header-icons">
+      <nav>
+        @for (item of headerItems; track item.label) {
+        <app-nav-button [icon]="item.icon" [class]="item.class" />
+        }
+      </nav>
+    </div>
   </header>
     `,
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  protected readonly headerItems = [
+    { label: 'Support', class: 'support', icon: 'support' },
+    { label: 'Inställningar', class: 'settings', icon: 'settings' },
+    { label: 'Googles appar', class: 'google-apps', icon: 'apps' },
+    { label: 'Google konto', class: 'google-account', icon: 'account' },
+  ];
 }
