43b61cc282a514cc71ab9eabd88add25fd4f43fe
skogsfrans <156783701+skogsfrans@users.noreply.github.com>
2026-01-14 16:55:12 +0100

    Add logic for newContent comp to create input if file is selected

--- client/src/app/new-content/new-content.component.ts
+++ client/src/app/new-content/new-content.component.ts
@@ -1,29 +1,58 @@
-import { Component } from '@angular/core';
-import { FileuploadComponent } from '../components/fileupload/fileupload.component';
+import { Component, inject } from '@angular/core';
 import { FileHandlingService } from '../services/file-handling.service';
 
 @Component({
   selector: 'app-new-content',
   imports: [],
   template: ` <div>
-    <select name="new-content" class="new-content">
-      <option value="">Ladda upp ny mapp</option>
-      <option (click)="onClick()" value="">Ladda upp ny fil</option>
+    <select name="new-content" class="new-content" (change)="onSelectChange($event.target.value)">
+      <option value="folder">Ladda upp ny mapp</option>
+      <option value="file">Ladda upp ny fil</option>
     </select>
   </div>`,
   styleUrls: ['./new-content.component.scss'],
 })
 export class NewContentComponent {
-  private fileService = new FileHandlingService();
+  FileService = inject(FileHandlingService);
+
+  /*
+  1. vi hämtar att det är en "fil" från select
+  2. vi skapar en input som inte syns och där vi kan ladda upp filen från datorn
+  3. vi sparar filen här, lokalt
+  (4. vi skickar filen till service som uppdaterar selectedFile)
+  5. anropa (härifrån) att vi vill ladda upp filen 
+   */
+
+
+  private newFile = File; 
+
+  onSelectChange(event: Event) {
+    const select = event.target as HTMLSelectElement;
+    const value = select.value;
+    
+    if (value === "file") {
+      const input = document.createElement('input'); // This createElement use is approved by Oscar!
+      input.type = 'file';
+      input.style.display = 'none';
+      document.body.appendChild(input);
+      input.click();
+      document.body.removeChild(input);
+
+      if (!input.files || input.files.length === 0 ) {
+        alert("Fil kunde inte laddas upp");
+        return;
+      }
+
+      const fileToReturn: File = input.files[0];
+      this.FileService.onFileSelected(fileToReturn);
+    }
+
+
+
+  }
+
 
   onClick(): void {
-    const input = document.createElement('input'); // This createElement use is approved by Oscar!
-    input.type = 'file';
-    input.style.display = 'none';
-    document.body.appendChild(input);
-    input.click();
-    document.body.removeChild(input);
-    const fileToReturn: File | null = input.files ? input.files[0] : null;
-    this.fileService.onFileSelected(fileToReturn);
+
   }
 }
